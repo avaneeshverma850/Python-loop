978f09a4b2b68519a5d52952cb3c78b19d5e884f
Avaneesh verma <124239728+avaneeshverma850@users.noreply.github.com>
2024-01-04 22:18:09 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,48 @@
+#Natural Number 
+"""x=int(input("Enter number:"))
+print()
+i=1
+while i<=x:
+  print(i,end=' ')
+  i=i+"""
+#Method Second
+"""y=int(input("Enter Limit"))
+print()
+for i in range(1,y+1):
+  print(i,end=" ")"""
+#Sum of the first n natural number 
+"""x=int(input("Enter Number"))
+print()
+sum=0
+i=1
+while i<=x:
+  sum=sum+i
+  i=i+1
+print("Sum of natural number is:",sum)"""
+#Method Second
+"""x=int(input("Enter nuumber"))
+print()
+sum=0
+for i in range(1,x+1):
+  sum=sum+i
+print(sum)"""
+#Reversing a number
+x=int(input("Enter Number:"))
+print()
+Rev=0
+while x>0:
+  Rev=Rev*10+x%10
+  x=x//10
+print(Rev)
+
+
+
+
+
+
+
+
+
+
+
+
